468cd52960d47e99b4f48ef8c27030f636a26b46
pikaqi-dot <hual83129@gmail.com>
2022-06-24 20:38:04 +0800

    使用goland上的git功能

--- 7.go
+++ 7.go
@@ -23,32 +23,99 @@
 //func test(ch <-chan int)
 //上述代码限定在 test 函数中只能从 ch 通道读取 int 类型数据。
 //
-//虽然我们也可以像声明正常通道类型那样声明单向通道，但我们一般不这么做，因为这样一来，就是从语法上限定通道的操作类型了，对于只读通道只能接收数据，对于只写通道只能发送数据：
+//虽然我们也可以像声明正常通道类型那样声明单向通道，但我们一般不这么做，因为这样一来，就是从语法上限定通道的操作类型了，对于只读通道
+//只能接收数据，对于只写通道只能发送数据：
 //
-//1
+//
 //var ch1 chan int
-//2
+//
 //var ch2 chan<- int
-//3
+//
 //var ch3 <-chan int
 //单向通道的初始化和双向通道一样：
 //
-//1
+//
 //ch1 := make(chan int)
-//2
+//
 //ch2 := make(chan<- int)
-//3
+//
 //ch3 := make(<-chan int)
 //此外，我们还可以通过如下方式实现双向通道和单向通道的转化：
 //
-//1
+//
 //ch1 := make(chan int)
-//2
+//
 //ch2 := <-chan int(ch1)
-//3
+//
 //ch3 := chan<- int(ch1)
-//基于双向通道 ch1，我们通过类型转化初始化了两个单向通道：单向只读的 ch2 和单向只写的 ch3。注意这个转化是不可逆的，双向通道可以转化为任意类型的单向通道，但单向通道不能转化为双向通道，读写通道之间也不能相互转化。
+//基于双向通道 ch1，我们通过类型转化初始化了两个单向通道：单向只读的 ch2 和单向只写的 ch3。注意这个转化是不可逆的，双向通道可以转化为任意
+//类型的单向通道，但单向通道不能转化为双向通道，读写通道之间也不能相互转化。
 //
 //实际上，我们在将双向通道传递到限定通道参数操作类型的函数时，就应用到了类型转化。
 //
 //我们可以通过单向通道来约束上篇教程的示例代码中子协程对通道的单向写入操作：
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func test(ch chan<- int) {
+	for i := 0; i < 100; i++ {
+		ch <- i
+	}
+	close(ch)
+}
+
+func main() {
+	start := time.Now()
+	ch := make(chan int, 20)
+	go test(ch)
+	for i := range ch {
+		fmt.Println("接收到的数据:", i)
+	}
+	end := time.Now()
+	consume := end.Sub(start).Seconds()
+	fmt.Println("程序执行耗时(s)：", consume)
+}
+
+
+//如果我们将 test 函数中的通道参数类型约束调整为 test(ch <-chan int)，编译代码就会报错：
+//
+//
+//# command-line-arguments
+//
+//./channel3.go:10:12: invalid operation: ch <- i (send to receive-only type <-chan int)
+//
+//./channel3.go:12:10: invalid operation: close(ch) (cannot close receive-only channel)
+//提示传入的通道是只读通道（receive-only channel），不能进行写入操作，此外，关闭通道函数 close 也不能作用到只读通道。
+//
+//如果将 main 函数中的通道初始化语句修改为 ch := make(chan<- int)，编译时也会报错：
+//
+//
+//# command-line-arguments
+//
+//./channel3.go:19:14: invalid operation: range ch (receive from send-only type chan<- int)
+//提示不能通过 range 语句从只写通道（send-only）中接收数据。
+//
+//我们也可以定义一个返回值类型为单向只读通道的函数，以便得到该返回值的代码只能从通道中接收数据：
+//
+//
+//func test() <-chan int {
+//
+//    ch := make(chan int, 20)
+//
+//    for i := 0; i < 100; i++ {
+//
+//        ch <- i
+//
+//    }
+//
+//    close(ch)
+//
+//    return ch
+//
+//}
+//显然，合理使用单向通道，可以有效约束不同业务对通道的操作，避免越权使用和滥用，此外，也提高了代码的可读性，一看函数参数就可以判断
+//出业务对通道的操作类型。
